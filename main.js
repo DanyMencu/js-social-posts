cf478d4d252379fbb95e0b02504b36b8d30b924d
DanyMencu <dany69men@gmail.com>
2021-11-05 15:37:54 +0100

    Draft project upload with array

---
+++ main.js
@@ -0,0 +1,49 @@
+//* 1.Creiamo il nostro array di 5 oggetti che rappresentano ciascun post.
+
+//*2.Ogni post dovrà avere le informazioni necessarie per stampare la relativa card: nome autore, foto profilo, data, testo del post, immagine, numero di likes.
+
+//*3.Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
+
+//1.
+const posts = [
+    {
+        autorName: "Gigi D'Alessio",
+        profileImg: 'https://picsum.photos/200',
+        date: '2 mesi fa',
+        postText: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        imgPost: 'https://picsum.photos/600/350',
+        likesCounter: '125',
+    },
+    {
+        autorName: "Nino D'Angelo",
+        profileImg: 'https://picsum.photos/200',
+        date: '4 mesi fa',
+        postText: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        imgPost: 'https://picsum.photos/600/350',
+        likesCounter: '52',
+    },
+    {
+        autorName: 'Nico Pandetta',
+        profileImg: 'https://picsum.photos/200',
+        date: '3 mesi fa',
+        postText: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        imgPost: 'https://picsum.photos/600/350',
+        likesCounter: '36',
+    },
+    {
+        autorName: 'LIBERATO',
+        profileImg: 'https://picsum.photos/200',
+        date: '8 mesi fa',
+        postText: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        imgPost: 'https://picsum.photos/600/350',
+        likesCounter: '75',
+    },
+    {
+        autorName: 'Peppe Sokx',
+        profileImg: 'https://picsum.photos/200',
+        date: '125 mesi fa',
+        postText: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        imgPost: 'https://picsum.photos/600/350',
+        likesCounter: '1',
+    },
+];
